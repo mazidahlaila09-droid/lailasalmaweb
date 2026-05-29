667725726678f7b743055e8f0643c0af7fe7745d
mazidahlaila09-droid <mazidahlaila09@gmail.com>
2026-05-30 05:56:44 +0700

    Add files via upload

---
+++ login/js/login.js
@@ -0,0 +1,33 @@
+document.getElementById("loginForm").addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    const res = await fetch("https://herisusanta.my.id/javalogin/api/auth.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: `action=login&username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
+    });
+
+    const data = await res.json();
+
+    if (data.status === "success") {
+        // Simpan username ke localStorage
+        localStorage.setItem("username", data.username);
+        
+        // JEMBATAN KELUAR FOLDER: Melompat ke landingpage.html yang ada di folder luar
+        window.location.href = "../landingpage.html";
+         
+    } else {
+        const alertBox = document.getElementById("alertBox");
+        alertBox.innerText = "Username atau Password salah, silahkan coba lagi";
+        alertBox.style.display = "block";
+
+        setTimeout(() => {
+            alertBox.style.display = "none";
+        }, 3000);
+    } 
+});
